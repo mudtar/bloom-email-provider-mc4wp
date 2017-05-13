0cfa60aaabb53d143ef01d3cb5d3e25639077157
Ian Burton <ian.burton@sportscarmarket.com>
2017-05-12 21:01:20 -0700

    Properly initialize and authorize bloom account

--- bloom-email-provider-mc4wp.php
+++ bloom-email-provider-mc4wp.php
@@ -9,8 +9,24 @@ if ( !defined( 'ABSPATH' ) ) {
 function bloom_mc4wp_initialize_component( $third_party_components, $group ) {
     include_once __DIR__ . '/core/components/api/email/MC4WP.php';
 
-    $third_party_components['mc4wp'] = new ET_Core_API_Email_MC4WP;
+    $third_party_components['mc4wp'] = new ET_Core_API_Email_MC4WP( 'bloom', 'default' );
 
     return $third_party_components;
 }
 add_filter( 'et_core_get_third_party_components', 'bloom_mc4wp_initialize_component', 10, 2 );
+
+function bloom_mc4wp_initially_authorize_default_account() {
+    // Only run this in the administration panel to avoid unnecessary
+    // frontend overhead.
+    if ( is_admin() ) {
+        $providers = new ET_Core_API_Email_Providers;
+        $provider = $providers->get( 'mc4wp', 'default', 'bloom' );
+
+        if ( $provider && !$provider->is_authenticated() ) {
+            $provider->data['is_authorized'] = true;
+            $provider->save_data();
+            do_action( 'bloom_lists_auto_refresh' );
+        }
+    }
+}
+add_filter( 'init', 'bloom_mc4wp_initially_authorize_default_account' );
